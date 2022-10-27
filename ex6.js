2805aec40438e64b2d73c3c94de8fe8f529f18fa
celiceceron <celice.cer@gmail.com>
2022-10-28 01:10:27 +0200

    codigo usando slice e push

---
+++ ex6.js
@@ -0,0 +1,22 @@
+function geraGrupoDe2 (lista) {
+    const qtdGrupos = Math.floor(lista.length/2);
+    let grupos = [];
+
+    for (let i = 0; i < qtdGrupos; i++){
+        const indiceInicio = i * 2;
+        const indiceFinal = i * 2 +2;
+        grupos.push(lista.slice(indiceInicio, indiceFinal));
+
+    }
+
+    if (lista.length % 2 != 0) {
+        const indiceUltimoGrupo = grupos.length - 1;
+        const indiceUltimaPessoa = lista.length - 1;
+        grupos[indiceUltimoGrupo].push(lista[indiceUltimaPessoa]);
+    }
+    return grupos;
+}
+
+let l = ["Anderson Luz", "Fabio Vechia", "Claudia Lopes", "Rafael Alves", "Maria Mansano", "Jairo Ferreira", "Vitor Ribeiro"];
+
+console.table(geraGrupoDe2(l));
